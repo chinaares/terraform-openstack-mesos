2f0c8b8ebb93b06390ce042d069c5ee7cdfcd661
Santhosh Sundararaman <santhoshsram@gmail.com>
2016-02-11 22:25:14 -0800

    update master playbook to create mesos and zk conf files

--- mesos-openstack/scripts/update-inventory-masters.sh
+++ mesos-openstack/scripts/update-inventory-masters.sh
@@ -2,8 +2,36 @@
 
 # Input Args: List of IP addresses separated by space.
 
+#
+# Create the ansible inventory file for masters
+# We'll use inventory variables to assign zookeeper
+# ids fo the master. (Ansible is neat like that!)
+#
+zk_id=1
 echo "[masters]" > ~/ansible/inventory/masters
 for master in "$@";
 do
-   echo "$master" >> ~/ansible/inventory/masters
+   echo "$master zk_id=1" >> ~/ansible/inventory/masters
+   zk_id=$((zk_id+1))
 done
+
+#
+# Create config for /etc/mesos/zk
+#
+zk="zk://"
+for master in "$@";
+do
+   zk="${zk}${master}:2181,"
+done
+
+# Remove last trailing comma
+zk=${zk::-1}
+
+# Add the suffix /mesos
+zk="${zk}/mesos"
+
+# Write the config to the template file
+echo "$zk" > ~/ansible/templates/master_etc_mesos_zk
+
+# Create a template file for /etc/zookeeper/conf/myid
+echo "{{ zk_id }}" > ~/ansible/templates/master_etc_zookeeper_conf_myid.j2
