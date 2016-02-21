7b5dd93aafce7d600f9c92ee9013726602906370
Santhosh Sundararaman <santhoshsram@gmail.com>
2016-02-20 22:48:15 -0800

    update bootstap-master to create marathon & mesos configs

--- mesos-openstack/scripts/update-inventory-masters.sh
+++ mesos-openstack/scripts/update-inventory-masters.sh
@@ -11,7 +11,7 @@ zk_id=1
 echo "[masters]" > ~/ansible/inventory/masters
 for master in "$@";
 do
-   echo "$master zk_id=1" >> ~/ansible/inventory/masters
+   echo "$master zk_id=$zk_id" >> ~/ansible/inventory/masters
    zk_id=$((zk_id+1))
 done
 
@@ -25,10 +25,36 @@ do
 done
 
 # Remove last trailing comma
+# We'll use the config in zk for marathon config file too.
 zk=${zk::-1}
 
-# Add the suffix /mesos
-zk="${zk}/mesos"
-
 # Write the config to the template file
-echo "$zk" > ~/ansible/templates/master_etc_mesos_zk
+echo "$zk/mesos" > ~/ansible/templates/master_etc_mesos_zk
+
+#
+# Create config for /etc/zookeeper/conf/zoo.cfg
+#
+zk_id=1
+truncate ~/ansible/templates/master_etc_zookeeper_conf_zoo_cfg
+for master in "$@";
+do
+   echo "server.$zk_id=$master:2888:3888" >> ~/ansible/templates/master_etc_zookeeper_conf_zoo_cfg
+   zk_id=$((zk_id+1))
+done
+
+#
+# Create config for /etc/mesos-master/quorum
+#
+quorum="$((($#+1)/2))"
+echo $quorum > ~/ansible/templates/master_etc_mesos_master_quorum
+
+#
+# Create config for /etc/marathon/conf/master
+#
+# No need to create a new file. We'll just re-use
+# ~/ansible/templates/master_etc_mesos_zk
+
+#
+# Create config for /etc/marathon/conf/zk
+#
+echo "$zk/marathon" > ~/ansible/templates/master_etc_marathon_conf_zk
