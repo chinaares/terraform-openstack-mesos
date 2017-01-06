a7a2edc8edf041134ca66d1303d9861b993e85f7
Ares <yangzibin.cn@hotmail.com>
2017-01-06 16:50:25 +0800

    apt-get only install ansible 1.5.x version, manually install 1.9.4 version
    Signed-off-by: Ares <yangzibin.cn@hotmail.com>

--- scripts/prep-config-server.sh
+++ scripts/prep-config-server.sh
@@ -4,7 +4,27 @@
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update -y
-sudo apt-get install -y ansible
+#sudo apt-get install -y ansible
+#(install newer version ansible)
+#wget https://launchpad.net/~ansible/+archive/ubuntu/ansible-1.9/+files/ansible_1.9.4-1ppa~trusty_all.deb -O /tmp/ansible.deb
+wget http://192.161.14.24/download/ansible_1.9.4-1_all.deb -O /tmp/ansible.deb
+
+# Manually install ansible deps
+sudo apt-get install -y python-support \
+  python-jinja2 \
+  python-yaml \
+  python-paramiko \
+  python-httplib2 \
+  python-crypto sshpass \
+  ipython \
+  python-netaddr-docs \
+  python-netaddr
+
+# Install ansible Package
+sudo dpkg -i /tmp/ansible.deb
+
+# Remove downloaded ansible package
+rm -f /tmp/ansible.deb
 
 # Create & prep workspace for ansible configs & playbooks
 mkdir -p ~/ansible/inventory
