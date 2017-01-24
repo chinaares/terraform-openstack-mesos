92833268c0b8b0a95e69d58f2a4cfb0e9c7a1b18
Ares <yangzibin.cn@hotmail.com>
2017-01-24 18:35:10 +0800

    Add search domain novalocal, update /etc/resolv.conf Install DNS Server and DNS Api Service
    Signed-off-by: Ares <yangzibin.cn@hotmail.com>

--- scripts/prep-config-server.sh
+++ scripts/prep-config-server.sh
@@ -37,3 +37,63 @@ mkdir -p ~/ansible/inventory
 touch ~/ansible/inventory/masters
 touch ~/ansible/inventory/slaves
 
+# Add search domain, update /etc/resolv.conf
+echo 'search novalocal' | sudo tee /etc/resolvconf/resolv.conf.d/tail && sudo resolvconf -u
+# Install DNS Server For vm
+sudo apt-get install -y bind9 bind9-host dnsutils
+# todo configure ....
+sudo mv /home/ubuntu/confs/dns/named.conf.novalocal /etc/bind/named.conf.novalocal
+echo "include \"/etc/bind/named.conf.novalocal\";" | sudo tee -a /etc/bind/named.conf
+echo "options {
+        directory \"/var/cache/bind\";
+
+        // If there is a firewall between you and nameservers you want
+        // to talk to, you may need to fix the firewall to allow multiple
+        // ports to talk.  See http://www.kb.cert.org/vuls/id/800113
+
+        // If your ISP provided one or more IP addresses for stable
+        // nameservers, you probably want to use them as forwarders.
+        // Uncomment the following block, and insert the addresses replacing
+        // the all-0's placeholder.
+
+        forwarders {
+                192.168.1.12;
+                202.106.0.20;
+        };
+
+        //========================================================================
+        // If BIND logs error messages about the root key being expired,
+        // you will need to update your keys.  See https://www.isc.org/bind-keys
+        //========================================================================
+        dnssec-validation auto;
+
+        auth-nxdomain no;    # conform to RFC1035
+        listen-on-v6 { any; };
+};" | sudo tee /etc/bind/named.conf.options
+sudo chown bind:bind /home/ubuntu/confs/dns/db.*
+sudo mv /home/ubuntu/confs/dns/db.* /var/cache/bind/
+sudo service bind9 restart
+netstat -nlt
+
+gem sources --add https://gems.ruby-china.org/ --remove http://rubygems.org/
+gem sources -l
+sudo gem install sinatra json ipaddr
+echo '#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+ruby /home/ubuntu/confs/dns/dns.rb -p 1080 -o 0.0.0.0 >/var/log/dnsapi_log 2>&1 &
+sleep 10
+~/scripts/regist-to-dns.sh
+exit 0' | sudo tee /etc/rc.local
+sudo /etc/init.d/rc.local start
+netstat -nlt
